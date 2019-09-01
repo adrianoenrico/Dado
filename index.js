6aff2abfa91f2cc583c08dd9fbf15ae7285c83b4
Adriano Enrico <adrianoenrico1@gmail.com>
2019-09-02 00:00:14 +0100

    Updated from logic to string manip

--- index.js
+++ index.js
@@ -1,45 +1,17 @@
-var randomNumber1;
+var randomNumber1, randomNumber2, img1,img2;
 randomNumber1 = Math.random();
 randomNumber1 = 6*(randomNumber1) + 1;
-randomNumber1 = Math.round(randomNumber1);
+randomNumber1 = Math.floor(randomNumber1);
 
-if (randomNumber1 == 1){
-    document.querySelector(".img1").setAttribute("src", "images/dice1.png");
-}
-else if (randomNumber1 == 2) {
-    document.querySelector(".img1").setAttribute("src", "images/dice2.png");
-}
-else if (randomNumber1 == 3) {
-    document.querySelector(".img1").setAttribute("src", "images/dice3.png");
-}
-else if (randomNumber1 == 4) {
-    document.querySelector(".img1").setAttribute("src", "images/dice4.png");
-}
-else if (randomNumber1 == 5) {
-    document.querySelector(".img1").setAttribute("src", "images/dice5.png");
-}
-
-var randomNumber2;
 randomNumber2 = Math.random();
 randomNumber2 = 6 * (randomNumber2) + 1;
-randomNumber2 = Math.round(randomNumber2);
+randomNumber2 = Math.floor(randomNumber2);
 
+img1 = "images/dice" + randomNumber1 + ".png";
+img2 = "images/dice" + randomNumber2 + ".png";
 
-if (randomNumber2 == 1) {
-    document.querySelector(".img2").setAttribute("src", "images/dice1.png");
-}
-else if (randomNumber2 == 2) {
-    document.querySelector(".img2").setAttribute("src", "images/dice2.png");
-}
-else if (randomNumber2 == 3) {
-    document.querySelector(".img2").setAttribute("src", "images/dice3.png");
-}
-else if (randomNumber2 == 4) {
-    document.querySelector(".img2").setAttribute("src", "images/dice4.png");
-}
-else if (randomNumber2 == 5) {
-    document.querySelector(".img2").setAttribute("src", "images/dice5.png");
-}
+document.querySelector(".img1").setAttribute("src", img1);
+document.querySelector(".img2").setAttribute("src", img2);
 
 if(randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerText = "🎈 Jogador 1 vence!!";
